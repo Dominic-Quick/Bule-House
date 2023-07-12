7983e1c552471430a07a8a0fb7fd6ad649710bb4
Dominic <dominic101500@gmail.com>
2023-07-12 11:04:59 -0500

    added tb scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import "./App.css";
+import { useInView } from "react-intersection-observer";
+
+
 
 function App() {
+
+  const {ref: myRef, inView: visEl} = useInView({threshold:.6})
+  
   return (
     <>
-      <section id="top-bar-con">
+      <section id={`${visEl ? 'top-bar-con':'colorbar'}`}>
         <div id="l-con">
-          <div id="logo">
+          <div id='logo-con'>
             <img
-              src="src/photos/Screenshot 2023-07-06 at 10.21.34 PM.jpeg"
-              id="logo"
+              src="src/photos/Logo-Photo.png"
+              id={`${visEl ? 'logo':'logo-small'}`}
             />
           </div>
         </div>
@@ -27,23 +33,25 @@ function App() {
       </section>
       <div id="body-container">
         <div id="body">
-          <section className="sec-cons" id="top-sec">
+          <section ref={myRef}  className="sec-cons" id="top-sec">
             <div className="sec-top-con" id="ts">
               <img src="./src/photos/animals-1851535.jpg" id="ts-img" />
-            </div>
-            <div className="sec-bot-con" id="bs">
+              <div className="sec-bot-con" id="bs">
               <div id="crsl-con">
                 <div id="crsl">
                   <div className="crsl-dots" id="dot1"></div>
                   <div className="crsl-dots" id="dot2"></div>
                   <div className="crsl-dots" id="dot3"></div>
                 </div>
+              </div>
+              <div id="main-text">
                 <h1 id="ts-text">
-                  Example text whatever dominics uncle wants to put or get
-                  something off google, it doesnt matter.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id diam placerat, maximus purus nec, ultrices nisl. Vestibulum sit amet placerat ante. Mauris ac rhoncus lorem, nec interdum lacus. Quisque in mollis felis. Maecenas id velit in tortor bibendum vestibulum a non velit. Nunc finibus venenatis urna, sed suscipit mauris iaculis ut. Integer bibendum, nisi in efficitur malesuada, odio arcu rhoncus lorem, at lacinia leo urna ac tortor. Aenean mollis eros erat, non interdum sem tincidunt sed. Quisque vel pretium est, vulputate aliquet ex.
                 </h1>
               </div>
             </div>
+            </div>
+  
           </section>
 
           <section className="sec-cons">
