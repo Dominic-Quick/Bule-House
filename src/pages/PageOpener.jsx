8296e9b0f03c56c527b9cbd81515ffa6fc1321b6
Anthony Quick <quickanthony314@gmail.com>
2023-07-13 13:40:54 -0500

    broke down page opener

---
+++ src/pages/PageOpener.jsx
@@ -0,0 +1,25 @@
+export default function PageOpener({myRef}){
+    return (
+        <>
+        <section ref={myRef}  className="sec-cons" id="top-sec">
+            <div className="sec-top-con" id="ts">
+              <img src="./src/photos/animals-1851535.jpg" id="ts-img" />
+              <div className="sec-bot-con" id="bs">
+              <div id="crsl-con">
+                <div id="crsl">
+                  <div className="crsl-dots" id="dot1"></div>
+                  <div className="crsl-dots" id="dot2"></div>
+                  <div className="crsl-dots" id="dot3"></div>
+                </div>
+              </div>
+              <div id="main-text">
+                <h1 id="ts-text">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id diam placerat, maximus purus nec, ultrices nisl. Vestibulum sit amet placerat ante. Mauris ac rhoncus lorem, nec interdum lacus. Quisque in mollis felis. Maecenas id velit in tortor bibendum vestibulum a non velit. Nunc finibus venenatis urna, sed suscipit mauris iaculis ut. Integer bibendum, nisi in efficitur malesuada, odio arcu rhoncus lorem, at lacinia leo urna ac tortor. Aenean mollis eros erat, non interdum sem tincidunt sed. Quisque vel pretium est, vulputate aliquet ex.
+                </h1>
+              </div>
+            </div>
+            </div>
+          </section>
+        </>
+    )
+}
--- src/pages/homePage/App.jsx
+++ src/pages/homePage/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { useInView } from "react-intersection-observer";
 import TopBar from "./TopBar";
+import PageOpener from "../PageOpener";
 
 
 
@@ -15,25 +16,7 @@ function App() {
       <TopBar visEl = {visEl}/>
       <div id="body-container">
         <div id="body">
-          <section ref={myRef}  className="sec-cons" id="top-sec">
-            <div className="sec-top-con" id="ts">
-              <img src="./src/photos/animals-1851535.jpg" id="ts-img" />
-              <div className="sec-bot-con" id="bs">
-              <div id="crsl-con">
-                <div id="crsl">
-                  <div className="crsl-dots" id="dot1"></div>
-                  <div className="crsl-dots" id="dot2"></div>
-                  <div className="crsl-dots" id="dot3"></div>
-                </div>
-              </div>
-              <div id="main-text">
-                <h1 id="ts-text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id diam placerat, maximus purus nec, ultrices nisl. Vestibulum sit amet placerat ante. Mauris ac rhoncus lorem, nec interdum lacus. Quisque in mollis felis. Maecenas id velit in tortor bibendum vestibulum a non velit. Nunc finibus venenatis urna, sed suscipit mauris iaculis ut. Integer bibendum, nisi in efficitur malesuada, odio arcu rhoncus lorem, at lacinia leo urna ac tortor. Aenean mollis eros erat, non interdum sem tincidunt sed. Quisque vel pretium est, vulputate aliquet ex.
-                </h1>
-              </div>
-            </div>
-            </div>
-          </section>
+          <PageOpener myRef={myRef}/>
 
           <section className="sec-cons">
             <div className="sec-top-con" id="s-con">
